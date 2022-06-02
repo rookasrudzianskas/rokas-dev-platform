0295de756f4403ab0a579cc19206eb34ac80135c
Rokas <rookas.rudzenskas@gmail.com>
2022-06-02 17:11:05 +0300

    Rokas

--- Documents/fakerJS/write-to-database/pages/index.tsx
+++ Documents/fakerJS/write-to-database/pages/index.tsx
@@ -8,9 +8,55 @@ import {db} from "../firebase";
 const generate = require('project-name-generator');
 
 const Home: NextPage = () => {
-    const [number, setNumber] = useState(0);
+    // const [number, setNumber] = useState(0);
     // useEffect(() => {
-    //     faker.setLocale('ru');
+    //
+    //
+    // }, [number]);
+    //
+    // const runForLoop = () => {
+    //     for(let i = 0; i < 100; i++) {
+    //         // console.log(i);
+    //         setNumber(i);
+    //     }
+    // }
+    //
+    // useEffect(() => {
+    //     // runForLoop();
+    //     for(let i = 0; i < 100; i++) {
+    //         // console.log(i);
+    //         // setNumber(i);
+    //         faker.setLocale('en');
+    //         const uploadToTheFirebase = async () => {
+    //             const formEmail = faker.internet.email();
+    //             const formUserUid = `${Math.random().toString(36).substr(2, 9)}-wirooom@${formEmail}`;
+    //
+    //             // let match = formEmail.match(/^(\w+)\..*\.(\w+)@/);
+    //             // let empname = match[1];
+    //             // let emplname = match[2];
+    //
+    //             const newObj = {
+    //                 fullName: formEmail,
+    //                 userEmail: formEmail,
+    //                 userUid: formUserUid,
+    //                 userImage: faker.image.people(),
+    //                 userName: formEmail,
+    //                 userPlan: 'freePlan',
+    //                 userRole: 'user',
+    //                 tokenExpiration: faker.date.future(),
+    //                 token: faker.random.alphaNumeric(10),
+    //                 createdAt: serverTimestamp(),
+    //             };
+    //
+    //             await setDoc(doc(db, 'users', formUserUid), newObj);
+    //
+    //         }
+    //         uploadToTheFirebase();
+    //     }
+    // }, [])
+
+    // useEffect(() => {
+    //     faker.setLocale('en');
     //     const uploadToTheFirebase = async () => {
     //         const formEmail = faker.internet.email();
     //         const formUserUid = `${Math.random().toString(36).substr(2, 9)}-wirooom@${formEmail}`;
@@ -20,6 +66,42 @@ const Home: NextPage = () => {
     //         // let emplname = match[2];
     //
     //         const newObj = {
+    //             API_KEY: 'sfskjdaslkdjaskldjlkjsdshgisdyfewrjk@-wirooom',
+    //             MEDIAN_RESPONSE: 0,
+    //             REQUESTS: 0,
+    //             THROUGHPUT_LIMITED: 0,
+    //             THROUGHPUT_UNLIMITED: 0,
+    //             appID: `${Math.random().toString(36).substr(2, 9)}-wirooom`,
+    //             authorUid: formUserUid,
+    //             projectChain: 'projectChain',
+    //             projectName: generate().spaced,
+    //             projectDescription: faker.lorem.paragraph(),
+    //             projectEcosystem: 'Ethereum',
+    //             projectNetwork: 'Ropsten',
+    //             projectStatus: 'active',
+    //             projectType: 'private',
+    //             projectUid: `${Math.random().toString(36).substr(2, 9)}-wirooom`,
+    //             projectUrl: 'https://wirooom.com',
+    //             projectVersion: '1.0.0',
+    //             projectWebsite: 'https://wirooom.com',
+    //             projectCreatedAt: serverTimestamp(),
+    //             projectUpdatedAt: serverTimestamp(),
+    //             projectCreatedBy: formUserUid,
+    //             projectUpdatedBy: formUserUid,
+    //             projectCreatedByName: formEmail,
+    //             projectUpdatedByName: formEmail,
+    //             projectCreatedByImage: faker.image.people(),
+    //             projectUpdatedByImage: faker.image.people(),
+    //             projectCreatedByRole: 'user',
+    //             projectUpdatedByRole: 'user',
+    //             projectCreatedByPlan: 'freePlan',
+    //             projectUpdatedByPlan: 'freePlan',
+    //             projectCreatedByToken: faker.random.alphaNumeric(10),
+    //             projectUpdatedByToken: faker.random.alphaNumeric(10),
+    //             projectCreatedByTokenExpiration: faker.date.future(),
+    //             projectUpdatedByTokenExpiration: faker.date.future(),
+    //             projectCreatedByTokenUpdatedAt: serverTimestamp(),
+    //             createdAt: serverTimestamp(),
     //             fullName: formEmail,
     //             userEmail: formEmail,
     //             userUid: formUserUid,
@@ -29,10 +111,9 @@ const Home: NextPage = () => {
     //             userRole: 'user',
     //             tokenExpiration: faker.date.future(),
     //             token: faker.random.alphaNumeric(10),
-    //             createdAt: serverTimestamp(),
     //         };
     //
-    //         await setDoc(doc(db, 'users', formUserUid), newObj);
+    //         await setDoc(doc(db, 'projects', formUserUid), newObj);
     //
     //     }
     //     uploadToTheFirebase();
@@ -50,81 +131,47 @@ const Home: NextPage = () => {
     //     runForLoop();
     // }, [number])
 
-    useEffect(() => {
-        faker.setLocale('ru');
-        const uploadToTheFirebase = async () => {
-            const formEmail = faker.internet.email();
-            const formUserUid = `${Math.random().toString(36).substr(2, 9)}-wirooom@${formEmail}`;
 
-            // let match = formEmail.match(/^(\w+)\..*\.(\w+)@/);
-            // let empname = match[1];
-            // let emplname = match[2];
-
-            const newObj = {
-                API_KEY: 'sfskjdaslkdjaskldjlkjsdshgisdyfewrjk@-wirooom',
-                MEDIAN_RESPONSE: 0,
-                REQUESTS: 0,
-                THROUGHPUT_LIMITED: 0,
-                THROUGHPUT_UNLIMITED: 0,
-                appID: `${Math.random().toString(36).substr(2, 9)}-wirooom`,
-                authorUid: formUserUid,
-                projectChain: 'projectChain',
-                projectName: generate().spaced,
-                projectDescription: faker.lorem.paragraph(),
-                projectEcosystem: 'Ethereum',
-                projectNetwork: 'Ropsten',
-                projectStatus: 'active',
-                projectType: 'private',
-                projectUid: `${Math.random().toString(36).substr(2, 9)}-wirooom`,
-                projectUrl: 'https://wirooom.com',
-                projectVersion: '1.0.0',
-                projectWebsite: 'https://wirooom.com',
-                projectCreatedAt: serverTimestamp(),
-                projectUpdatedAt: serverTimestamp(),
-                projectCreatedBy: formUserUid,
-                projectUpdatedBy: formUserUid,
-                projectCreatedByName: formEmail,
-                projectUpdatedByName: formEmail,
-                projectCreatedByImage: faker.image.people(),
-                projectUpdatedByImage: faker.image.people(),
-                projectCreatedByRole: 'user',
-                projectUpdatedByRole: 'user',
-                projectCreatedByPlan: 'freePlan',
-                projectUpdatedByPlan: 'freePlan',
-                projectCreatedByToken: faker.random.alphaNumeric(10),
-                projectUpdatedByToken: faker.random.alphaNumeric(10),
-                projectCreatedByTokenExpiration: faker.date.future(),
-                projectUpdatedByTokenExpiration: faker.date.future(),
-                projectCreatedByTokenUpdatedAt: serverTimestamp(),
-                createdAt: serverTimestamp(),
-                fullName: formEmail,
-                userEmail: formEmail,
-                userUid: formUserUid,
-                userImage: faker.image.people(),
-                userName: formEmail,
-                userPlan: 'freePlan',
-                userRole: 'user',
-                tokenExpiration: faker.date.future(),
-                token: faker.random.alphaNumeric(10),
-            };
-
-            await setDoc(doc(db, 'projects', formUserUid), newObj);
-
-        }
-        uploadToTheFirebase();
-
-    }, [number]);
-
-    const runForLoop = () => {
-        for(let i = 0; i < 100; i++) {
-            // console.log(i);
-            setNumber(i);
-        }
-    }
-
-    useEffect(() => {
-        runForLoop();
-    }, [number])
+    // useEffect(() => {
+    //     faker.setLocale('ru');
+    //     const uploadToTheFirebase = async () => {
+    //         const formEmail = faker.internet.email();
+    //         const formUserUid = `${Math.random().toString(36).substr(2, 9)}-wirooom@${formEmail}`;
+    //
+    //         // let match = formEmail.match(/^(\w+)\..*\.(\w+)@/);
+    //         // let empname = match[1];
+    //         // let emplname = match[2];
+    //
+    //         const newObj = {
+    //             fullName: formEmail,
+    //             userEmail: formEmail,
+    //             userUid: formUserUid,
+    //             userName: formEmail,
+    //             userRole: 'user',
+    //             tokenExpiration: faker.date.future(),
+    //             token: faker.random.alphaNumeric(10),
+    //             feedback: '',
+    //             feedbackType: 'userFeedback',
+    //             isNewUser: true,
+    //         };
+    //
+    //         await setDoc(doc(db, 'feedback', formUserUid), newObj);
+    //
+    //     }
+    //     uploadToTheFirebase();
+    //
+    // }, [number]);
+    //
+    // const runForLoop = () => {
+    //     for(let i = 0; i < 100; i++) {
+    //         // console.log(i);
+    //         setNumber(i);
+    //     }
+    // }
+    //
+    // useEffect(() => {
+    //     runForLoop();
+    // }, [number])
 
 
   return (
