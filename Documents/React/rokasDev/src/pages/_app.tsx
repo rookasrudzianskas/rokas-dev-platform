cb56c88ce6f87e9df9875458f6cf30f9c09ecee6
Rokas <rookas.rudzenskas@gmail.com>
2022-10-18 17:58:44 +0300

    Rokas

--- Documents/React/rokasDev/src/pages/_app.tsx
+++ Documents/React/rokasDev/src/pages/_app.tsx
@@ -3,9 +3,20 @@ import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 import * as gtag from '../lib/gtag';
 import '../styles/globals.css';
+import Router from 'next/router'
+import { useState } from 'react';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+
+  Router.events.on('routeChangeStart', () => NProgress.start());
+  Router.events.on('routeChangeComplete', () => NProgress.done());
+  Router.events.on('routeChangeError', () => NProgress.done());
+
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
